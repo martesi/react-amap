fb0fefaea6b9ff030827cc30bdcdb1130c5f486e
martes <ikenworthy@qq.com>
2024-07-05 00:13:55 +0800

    test(Marker): forcing refresh

--- packages/marker/src/index.tsx
+++ packages/marker/src/index.tsx
@@ -1,6 +1,6 @@
 /// <reference types="@uiw/react-amap-types" />
 
-import { useImperativeHandle, forwardRef } from 'react';
+import { useImperativeHandle, forwardRef, useEffect } from 'react';
 import { OverlayProps } from '@uiw/react-amap-map';
 import { useMarker } from './useMarker';
 
@@ -16,5 +16,9 @@ export interface MarkerProps extends OverlayProps, AMap.MarkerEvents, AMap.Marke
 export const Marker = forwardRef<MarkerProps & { marker?: AMap.Marker }, MarkerProps>((props, ref) => {
   const { marker, MarkerPortal } = useMarker(props);
   useImperativeHandle(ref, () => ({ ...props, marker }), [marker]);
+  useEffect(() => {
+    console.log('Marker: created');
+    return () => console.log('Marker: destroyed');
+  }, []);
   return <MarkerPortal>{props.children}</MarkerPortal>;
 });
